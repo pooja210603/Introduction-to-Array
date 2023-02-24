a16a5c78370a4a535df21314bef90ea5808b01f1
Pooja <125560933+pooja210603@users.noreply.github.com>
2023-02-24 23:16:17 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,36 +1,15 @@
 //WAP to accept input from user in an array and display the sum and average
-/*#include<stdio.h>
+#include<stdio.h>
 int main()
 {
     int i;float marks[5];float sum=0;
     for(i=0;i<5;i++)
     {
-        printf("Enter the %d value \n",(i+1));
-        scanf("%f",&marks[i]);
-        sum=sum+marks[i];
+        printf("Enter value %d \n",(i+1));
+        scanf("%f",&marks[i]); // Accepting user input
+        sum=sum+marks[i]; // sum of data entered or input
     }
-    printf("The average value is %f",sum/5);
+    printf("The average value is %f",sum/5); // average of the data
     
 
-}*/
-
-//WAP to reverse a string  taken from user
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char str[20];
-    int len;int i;
-    char arr[len];
-    printf("Enter the string to be reversed \n");
-    scanf("%s,&str");
-    len=strlen(str);
-    for(i=0;i<len;i++)
-    {
-        arr[i]=str[i];
-    }
-    for(i=len-1;i>=0;i--)
-    {
-        printf("%c",str[i]);
-    }
 }
